35b568a13c6d26958e3f975251e8ef475091f595
tmka5301-rgb <tmka5301@gmail.com>
2026-04-01 11:28:22 +0800

    tseg

--- src/components/steps/Success.jsx
+++ src/components/steps/Success.jsx
@@ -9,7 +9,7 @@ export const Success = ({ step, handleClick, formValues, setFormErrors }) => {
   return (
     <div className="flex flex-col justify-between p-8 bg-white rounded-lg">
       <div>
-        <img className="w-15 h-15" src="/pinecone.png" alt="" />
+        <img className="w-15 h-15" src="/Pinecone.png" alt="" />
       </div>
       <h2 className="text-[26px] font-bold">You're All Set 🔥</h2>
       <p className="text-[18px] text-gray-400">
